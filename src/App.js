0d150689cc09690c2beed33ce531ee135605aed9
Ricardo Canelas <ricardocanelas@gmail.com>
2020-10-19 08:24:18 -0300

    feat: create useToast hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useToast from "./useToast";
+
+const variants = `primary success danger warning info dark`.split(" ");
+
+const words = `people history way art world information two family government
+ health system computer meat year thanks music person reading method data food
+ understanding theory law bird literature problem software control knowledge
+ power ability economics love internet television monitor dogs`.split(" ");
+
+const randomWord = () => words[Math.floor(Math.random() * words.length)];
+const randomVariant = () =>
+  variants[Math.floor(Math.random() * variants.length)];
 
 function App() {
+  const { addToast } = useToast();
+
+  const handleClick = (diretion) => () => {
+    addToast({ title: randomWord(), variant: randomVariant() }, diretion, 5000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div style={{ marginTop: "44vh" }}>
+        <button onClick={handleClick("top")}>Top</button>
+        <button onClick={handleClick("top-left")}>Top-Left</button>
+        <button onClick={handleClick("top-right")}>Top-Right</button>
+        <button onClick={handleClick("bottom")}>Bottom</button>
+        <button onClick={handleClick("bottom-left")}>Bottom-Left</button>
+        <button onClick={handleClick("bottom-right")}>Bottom-Right</button>
+      </div>
     </div>
   );
 }
---
+++ src/useToast.js
@@ -0,0 +1,151 @@
+import React, { createContext, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+
+const ToastContext = createContext();
+
+function ToastProvider({
+  children,
+  DefaultToast = null,
+  portal = null,
+  maximum = 10,
+}) {
+  const timers = useRef({});
+  const index = useRef(0);
+  const [toasts, setState] = useState([]);
+  const DefaultToastComponent = DefaultToast || Alert;
+  const portalTarget = portal;
+
+  function generateId() {
+    index.current += 1;
+    return Number(index.current);
+  }
+
+  function createTimeout(toast, time) {
+    timers.current[toast.id] = setTimeout(() => removeToast(toast.id), time);
+  }
+
+  function removeTimeout(toastId) {
+    clearTimeout(timers.current[toastId]);
+    delete timers.current[toastId];
+  }
+
+  const addToast = (props, direction = "bottom", time = 5000) => {
+    const opts = { props, direction, time };
+
+    if (arguments.length === 0 || !props) return;
+    if (isNaN(Number(direction)) && arguments.length === 2)
+      opts.time = direction;
+
+    const id = generateId();
+
+    setState((prevState) => {
+      let newToasts = [].concat(
+        { props, id, direction, Component: DefaultToastComponent },
+        prevState || []
+      );
+      createTimeout({ props, id, direction }, time);
+      if (newToasts.length > maximum) newToasts.pop();
+
+      return newToasts;
+    });
+  };
+
+  const removeToast = (toastId) => {
+    removeTimeout(toastId);
+    setState((prevState) => {
+      const newToasts = prevState.filter(({ id }) => id !== toastId);
+      return newToasts;
+    });
+  };
+
+  const clearToasts = () => {
+    setState([]);
+  };
+
+  const getToasts = (direction = null) => {
+    if (!direction) return toasts;
+    return toasts.filter((toast) => toast.direction === direction);
+  };
+
+  return (
+    <ToastContext.Provider
+      value={{
+        toasts,
+        getToasts,
+        addToast,
+        removeToast,
+        clearToasts,
+      }}
+    >
+      {portalTarget && createPortal(<Toaster />, portalTarget)}
+      {!portalTarget && <Toaster />}
+      {children}
+    </ToastContext.Provider>
+  );
+}
+
+function useToast() {
+  const context = React.useContext(ToastContext);
+
+  if (context === undefined) {
+    throw new Error(`useToast must be used within a ToastProvider`);
+  }
+
+  return context;
+}
+
+const Alert = ({ props, remove }) => {
+  const handleRemove = () => {
+    remove();
+  };
+
+  const { title, variant = "primary" } = props;
+
+  return (
+    <div className={`toaster__message alert alert-${variant}`}>
+      {title}
+      <button
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
+        onClick={handleRemove}
+      >
+        <span aria-hidden="true">×</span>
+      </button>
+    </div>
+  );
+};
+
+const Toaster = () => {
+  const { removeToast, getToasts } = useToast();
+
+  const renderScreen = (direction) => {
+    return (
+      <span className={`toaster_screen-${direction}`}>
+        {getToasts(direction).map((toast, index) => (
+          <toast.Component
+            key={`${direction}-${index}`}
+            props={toast.props}
+            remove={() => removeToast(toast.id)}
+          />
+        ))}
+      </span>
+    );
+  };
+
+  return (
+    <div className="toaster">
+      {renderScreen("top")}
+      {renderScreen("top-left")}
+      {renderScreen("top-right")}
+      {renderScreen("bottom")}
+      {renderScreen("bottom-left")}
+      {renderScreen("bottom-right")}
+    </div>
+  );
+};
+
+export { ToastProvider, Alert, useToast };
+
+export default useToast;
